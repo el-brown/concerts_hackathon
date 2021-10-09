a5385b1d3625745a5667922e1571a37bc27c3ec5
Elise Brown <elisemarie88@gmail.com>
2021-10-09 15:51:43 -0600

    imported images

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import elise from '../EliseZoom.png';
 
 function About(props) {
+  console.log(elise)
   return (
     <div>
-      
+      <img src={logo} alt="Logo" />
     </div>
   );
 }
